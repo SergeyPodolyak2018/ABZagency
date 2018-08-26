af64c4020e908a6f9e8a40e913e6e81dfa11ceb2
SergeyPodolyak <sergey.podolyak2018@gmail.com>
2018-08-26 23:09:11 +0300

    Add heder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/services.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { FormComponent } from './form/form.component';
+import { WrapperFormComponent } from './wrapper-form/wrapper-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    WrapperComponent,
+    FormComponent,
+    WrapperFormComponent
   ],
   imports: [
     BrowserModule
